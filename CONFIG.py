47a0850d0880a8303bf32c003cfb8df9dc40961e
Alexey Shaytan <alex@intbio.org>
2018-02-10 16:27:11 +0300

    Update

--- CONFIG.py
+++ CONFIG.py
@@ -5,6 +5,6 @@ EMAIL = "alex@intbio.org"
 #This usually happens if they are installed through conda.
 
 TEMP_DIR=os.path.expanduser('~/temp')
-MUSCLE_BIN_PATH=os.path.join(sys.exec_prefix,'muscle')
-PATH_TO_AL2CO=os.path.join(sys.exec_prefix,'al2co')
+MUSCLE_BIN_PATH=os.path.join(sys.exec_prefix,os.path.join('bin','muscle'))
+PATH_TO_AL2CO=os.path.join(sys.exec_prefix,os.path.join('bin','al2co'))
 BLOSSUM_PATH=os.path.expanduser('~/soft/al2co/BLOSUM62.txt')
